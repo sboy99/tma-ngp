8690e1a6b5b6690a9a23d16d818181e6d2ffd215
Sagar Bera <contact.sagarbera@gmail.com>
2024-06-29 16:24:24 +0530

    feat: wallet connect complete

--- src/components/me.tsx
+++ src/components/me.tsx
@@ -1,14 +1,28 @@
 "use client";
 
+import { useAccount, useBalance } from "wagmi";
 import { useTma } from "./tma/hook";
+
+function formatBalance(balance?: bigint) {
+  if (!balance) return 0;
+  return (parseFloat(balance.toString()) / 10 ** 18).toFixed(4);
+}
 
 export function Me() {
   const { user } = useTma();
+  const { address } = useAccount();
+  const balance = useBalance({
+    address,
+  });
 
   return (
     <div className="">
       <pre>Name : {user.firstName}</pre>
       <pre>Username : @{user.username}</pre>
+      <pre>Address: {address}</pre>
+      <pre>
+        Balance: {formatBalance(balance.data?.value)} {balance.data?.symbol}{" "}
+      </pre>
     </div>
   );
 }
